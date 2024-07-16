0ef9065058384967ca4142b06362614ffb8d89c0
Kaiki Alvarenga de Souza <kaikialvarengasouza098@gmail.com>
2024-07-16 16:32:52 -0300

    Initial commit

---
+++ src/main/java/dio/diodesignpattern/DioDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package dio.diodesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/diodesignpattern/DioDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.diodesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
